53cb2f014249ad870aea0bb5f83e98c0f5e2f555
pallasuy <pallas5192@gmail.com>
2023-08-11 00:37:29 -0300

    Aplicando multiples Routers

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,7 +1,29 @@
+import { useNavigate } from "react-router"
 
 
 export const Login = () => {
+
+    const navigate = useNavigate();
+
+
+    const onLogin = () => {
+        navigate('/marvel', {
+            replace: true
+        });
+
+    }
+
     return (
-        <h1>Login</h1>
+        <>
+            <div className="container mt-5">
+                <h1>Login</h1>
+                <hr />
+                <button className="btn btn-primary"
+                    onClick={onLogin}>
+                    Login
+                </button>
+            </div>
+        </>
+
     )
 }
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,19 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/login', {
+
+            replace: true
+        });
+
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
 
@@ -53,7 +65,9 @@ export const Navbar = () => {
                         Gabriel
                     </span>
 
-                    <button className='nav-item nav-link btn'>
+                    <button className='nav-item nav-link btn'
+                        onClick={onLogout}
+                    >
                         Logout
                     </button>
 
