75b9f5a84bbe2d46deedffc7d8d9bb697c7ebd63
pallasyu <pallas5192@gmail.com>
2023-08-18 20:18:55 -0300

    NEW: HeroesCard y boton hacia atras

--- src/heroes/pages/Hero.jsx
+++ src/heroes/pages/Hero.jsx
@@ -1,20 +1,50 @@
 import React from 'react'
-import { Navigate, useParams } from 'react-router'
+import { Navigate, useNavigate, useParams } from 'react-router'
 import { getHeroById } from '../helpers';
 
 export const Hero = () => {
     const { id, ...rest } = useParams();
 
+    const navigate = useNavigate();
     const hero = getHeroById(id);
-    console.log(hero);
+
+    const onNavigateBack = () => {
+        navigate(-1);
+    }
 
 
     if (!hero) return (<Navigate to={'./marvel'}> </Navigate>)
 
     return (
-        <>
-            <h1>{hero.superhero} </h1>
-        </>
+        <div className='row mt-5'>
+            <div className="col-4">
+                <img
+                    src={`/assets/heroes/${id}.jpg`}
+                    alt={hero.superhero}
+                    className='img-thumbnail' />
+            </div>
+            <div className="col-8">
+                <h3>{hero.superhero} </h3>
+                <ul className='list-group list-group-flush' >
+                    <li className='list-group-item'> <b>Alter ego:</b> {hero.alter_ego}  </li>
+                    <li className='list-group-item'> <b>Publisher:</b> {hero.publisher}  </li>
+                    <li className='list-group-item'> <b>First appearence:</b> {hero.first_appearance}  </li>
+                </ul>
+
+                <h5 className='mt-3'>
+                    Characters
+                </h5>
+                <p> {hero.characters} </p>
+
+                <button className='btn btn-outline-primary'
+                    onClick={onNavigateBack}>
+                    Regresar
+                </button>
+
+            </div>
+
+
+        </div>
 
     )
 }
