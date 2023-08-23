bc948474516e5c71d94d0929a79edd8dc41c246e
pallasyu <pallas5192@gmail.com>
2023-08-22 23:04:04 -0300

    Definiendo rutas publicas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,19 @@ import { Route, Routes } from "react-router"
 import { HeroesRoutes } from "../heroes"
 import { Login } from "../auth"
 import { PrivateRoute } from "./PrivateRoute"
+import { PublicRoute } from "./PublicRoute"
 
 
 export const AppRouter = () => {
     return (
         <>
             <Routes>
-                <Route path="login" element={<Login />} />
+
+                <Route path="login" element={
+                    <PublicRoute>
+                        <Login />
+                    </PublicRoute>
+                } />
 
                 <Route path="/*" element={
                     <PrivateRoute>
