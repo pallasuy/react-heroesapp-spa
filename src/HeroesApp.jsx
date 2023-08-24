5d27ebc95e4f4176fb205d3964af1d3bf709b16b
pallasyu <pallas5192@gmail.com>
2023-08-23 22:38:12 -0300

    Quito cabezal

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -5,7 +5,6 @@ export const HeroesApp = () => {
     return (
         <>
             <AuthProvider>
-                <h1>HeroesApp</h1>
                 <AppRouter />
             </AuthProvider>
 
