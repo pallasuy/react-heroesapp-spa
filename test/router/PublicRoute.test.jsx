9d74f4d65cc205e2e49bafd22aadd3da89b337da
pallasuy <pallas5192@gmail.com>
2023-09-08 22:22:39 -0300

    ADD: Pruebas en el Public Route con context

--- test/router/PublicRoute.test.jsx
+++ test/router/PublicRoute.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import { AuthContext } from '../../src/auth'
 import { PublicRoute } from '../../src/router/'
+import { MemoryRouter, Route,Routes } from 'react-router-dom'
+
 
 describe('Pruebas sobre <PublicRouter/>', () => {
     test('Debe mostrar el children si no esta autenticado', () => {
@@ -18,7 +20,45 @@ describe('Pruebas sobre <PublicRouter/>', () => {
 
         expect(screen.getByText('Ruta Pública')).toBeTruthy();
 
-    })
+    });
+
+    test('Debe de navegar si esta autenticado', () => { 
+        const contextValue = {
+            logged: true,
+            user:{
+                name:'Juan',
+                id:'1234'
+            }
+        }
+
+        render(
+            <AuthContext.Provider value={contextValue} >
+                <MemoryRouter initialEntries={['/login']}>
+                    <Routes>
+                    <Route path='login' 
+                    element={
+                        <PublicRoute>
+                        <h1>Ruta Pública</h1>
+                    </PublicRoute>
+                    } />
+
+                    <Route path='marvel' element={
+                        <h1>Página Marvel</h1>
+                    }/>
+
+                    
+
+                    </Routes>
+               
+                </MemoryRouter>
+                
+            </AuthContext.Provider>
+        );
+
+        //screen.debug();
+        expect(screen.getByText('Página Marvel')).toBeTruthy();
+ 
+     })
 
 
 })
